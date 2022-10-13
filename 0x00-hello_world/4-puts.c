148f43764801d065985d354c2f993d464dadb0ef
20INNO <innocentoketcho200@>
2022-10-13 13:18:29 -0700

    (fail to plan then plan to fail)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * *main- Serves as the entry point for the program*
+ *Return: By default returns zero if no errors otherwise return non-zerovalue.
+ */
+int main(void)
+{
+puts("¥"Programming is like building amultilingual puzzel");
+return (0);
+}
