14e61521236da9d6734c900cee38c5a3934739b6
20INNO <innocentoketcho200@>
2022-10-13 13:33:52 -0700

    ( sleep stop)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main-entrypoint for the program.
+ * 
+ * Return:0 if no errors, return, return non zero of errors
+ * /
+ int main(void)
+{
+printf("with proper, but the outcome is apeice of art,¥");
+return(0);
+}
