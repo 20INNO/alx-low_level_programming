811f8bddc2edc7d8bb9c9ceef3d7056cdc499d3a
20INNO <innocentoketcho200@>
2022-10-16 03:35:07 -0700

    (Use the function puts)

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+# !/ bin / bash
+#include <stdio.h>
+/**
+ *main -is the entry point fir the program.* 
+ *Return:zero if no error, None-zero value if errors.*/
+int main(viod)
+{
+	printf("Size of a char: %ld byte(s)¥n", sizeof(char));
+	printf("size of an int: %ld byte(S)¥n", sizeof(int));
+	printf("size of a long int: %ld byte(S)¥n", iszeof(long int));
+	printf("size of a long long int: %ld byte(s)¥n", sizeof(long long int));
+	printf("size of afloat: %ld bye(s)¥n", sizeof(float));
+	return (0)
+}
