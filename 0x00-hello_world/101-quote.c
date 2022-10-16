c06dfbe969b7d6ae9f364ac5a2ea7d5766a12142
20INNO <innocentoketcho200@>
2022-10-16 11:09:33 -0700

    (wondreful)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<std.h>
+#include<stdio.h>
+
+/**
+ * main - print a piece of art
+ *
+ * Return: 1
+ */int main(void)
+{
+		printf(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19",59);
+	printf('\n');
+	return (1);
+}
+
