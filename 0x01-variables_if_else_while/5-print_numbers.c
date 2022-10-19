f1084757d2dcdcac107103b5582c65efc05e6a98
20INNO <innocentoketcho200@>
2022-10-19 05:32:19 -0700

    ( prints all single digit numbers of base 10 starting from 0)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0.
+ *
+ * return (0)
+ */
+{
+	int digitnum;
+
+	for (digitnum = 0; digitnum < 10; digitnum++)
+		printf("%d", digitnum);
+
+	printf("\n");
+
+	return (0);
+}
