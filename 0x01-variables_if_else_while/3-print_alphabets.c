22e3589de08193a05b808d9c889c60ecac331828
20INNO <innocentoketcho200@>
2022-10-19 01:05:49 -0700

    (prints the alphabet in lowercase, and then in uppercase)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: (0)
+ */
+int main(void)
+{
+	char alphaletter;
+
+	for (alphaletter = 'a'; letter <= 'z'; alphaletter++)
+		putchar(alphaletter);
+
+	for (alphaletter = 'A'; alphaletter <= 'Z'; letter++)
+		putchar(alphaletter);
+
+	putchar('\n');
+
+	return (0);
