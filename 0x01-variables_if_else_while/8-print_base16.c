f82b0cc17a903a0fcfef784d2786057e08edb9ad
20INNO <innocentoketcho200@>
2022-10-19 06:33:23 -0700

    (prints all the numbers of base 16 in lowercase)

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase.
+ *
+ * Return: (0).
+ */
+int main(void)
+{
+	char ch;
+	int num;
+
+	for (num = 0; num < 16; num++)
+		putchar((num % 16 + '0');
+
+		putchar(ch);
+
+		putchar('\n');
+
+		return (0);
+}
+
+
